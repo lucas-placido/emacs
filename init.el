cf302e78e04b37cb758cfb117798068114763c77
lucas-placido <lucas.cplacido@hotmail.com>
2024-02-10 15:42:15 -0300

    WIP - Add some Melpa packages
    Packages added: try, which-key, auto-complete, neotree

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-; Remover boas vindas					;
+; Precisa setar a variável HOME com o caminho onde a pasta .emacs fica para que emacs carregue o arquivo de configuração
+; SETX HOME C:\Users\%username%
+
+; Remover boas vindas
 (setq inhibit-startup-message t)
 
 ; Remover menu
@@ -11,5 +14,55 @@
 ; Inserir numero das linhas
 (global-display-line-numbers-mode 1)
 
-;; Tamanho da fonte
-(set-face-attribute 'default nil :height 120)
+; Tamanho da fonte
+(set-face-attribute 'default nil :height 110)
+
+; Pacotes
+(require 'package)
+(setq package-enable-at-startup nil) ; desativar ativação
+
+; MELPA
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize) ; iniciar pacotes
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package auto-complete
+  :ensure t
+  :init (progn
+	  (ac-config-default)
+	  (global-auto-complete-mode t)))
+
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+
+(use-package neotree
+  :ensure t
+  :bind (("C-\\" . 'neotree-toggle)))
+
+
+; Melpa stuff
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(all-the-icons neotree which-key try)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
