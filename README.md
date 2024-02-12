ea5e94a9f5aea5ada0f4d5eb51a3bcedb9b7833d
lucas-placido <lucas.cplacido@hotmail.com>
2024-02-11 23:57:20 -0300

    Add packages: elpy, flycheck

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # .emacs.d
 
 ## Setting up environmental variables
-* setx HOME C:/Users/%username/%
+* setx HOME C:/Users/%username/
+* setx $WORKON_HOME path # pyvenv
 
 ## Installing magit
 M-x package-install RET magit RET
--- init.el
+++ init.el
@@ -73,22 +73,44 @@
   :config (progn
 	    (global-set-key (kbd "C-c <down>") 'set-rectangular-region-anchor)
 	    (global-set-key (kbd "C-c <up>") 'set-rectangular-region-anchor)
-	    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-	    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-	    (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+	    (global-set-key (kbd "C-.") 'mc/mark-next-like-this)
+	    (global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
+	    (global-set-key (kbd "C-c C-.") 'mc/mark-all-like-this)))
 
-;; (use-package elpy
-;;   :ensure t
-;;   :init (elpy-enable))
-
-;; (use-package flycheck
-;;   :ensure t
-;;   :init (global-flycheck-mode t))
 
 ;; Python
-;; blacken
-;; py-autopep8
+;; (use-package pyvenv
+;;   :ensure t
+;;   :init (pyvenv-mode))
 
+;; (use-package blacken
+;;   :ensure t
+;;   :init (blacken-mode)
+;;   :hook (add-hook 'python-mode-hook 'blacken-mode))
+
+;; (use-package py-autopep8
+;;   :ensure t
+;;   :init (py-autopep8-mode)
+;;   :hook (add-hook 'python-mode-hook 'py-autopep8-mode))
+
+;; (use-package py-isort
+;;   :ensure t
+;;   :hook (add-hook 'before-save-hook 'py-isort-before-save))
+
+;; (use-package pytest
+;;   :ensure t)
+  
+
+(use-package elpy
+  :ensure t
+  :init (elpy-enable)
+  :config (progn
+	    (flymake-mode -1))
+  :hook (add-hook 'python-mode-hook 'blaken-mode))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode t))
 
 ; inicializa o pacote
 (when (< emacs-major-version 27)
@@ -118,6 +140,9 @@
 (global-set-key (kbd "C-z <right>") 'split-window-right)
 (global-set-key (kbd "C-z <down>") 'split-window-below)
 
+; Python
+(global-set-key (kbd "<f7>") (kbd "C-u C-c C-c"))
+
 ;;; Melpa stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -125,7 +150,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit elpy flycheck ace-window all-the-icons neotree which-key try)))
+   '(pytest blacken pyvenv pyenv magit elpy flycheck ace-window all-the-icons neotree which-key try)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
