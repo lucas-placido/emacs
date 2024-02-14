71f3560258713afe782a4a35b0261e8759ee2fae
lucas-placido <lucas.cplacido@hotmail.com>
2024-02-14 12:24:21 -0300

    Organize init.el & change font family

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Setting up environmental variables
 * setx HOME C:/Users/%username/
 * setx $WORKON_HOME path # pyvenv
+* Setar a versão do python que tem todas as ferramentas de linting para o flycheck conseguir acessar
 
 ## Installing magit
 M-x package-install RET magit RET
--- init.el
+++ init.el
@@ -1,6 +1,9 @@
-;;; init.el --- Initialization file for Emacs
-;;; Commentary: Emacs Startup File --- initialization for Emacs
+;;; package --- Summary
+;;; Commentary:
+;;; Emacs Startup File --- initialization for Emacs
 
+
+;;; Code:
 ;;; Configurações nativas
 ; Remover boas vindas
 (setq inhibit-startup-message t)
@@ -22,8 +25,7 @@
 (global-display-line-numbers-mode 1)
 
 ; Tamanho da fonte
-(set-face-attribute 'default nil :height 120 :family "Nimala UI") ; (font-family-list) C-j
-(setq custom-safe-themes t)
+(set-face-attribute 'default nil :height 100 :family "JetBrains Mono Medium") ; (font-family-list) C-j
 
 ; Tema
 ;; (load-theme 'misterioso)
@@ -48,6 +50,8 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+
+;; Utilizade
 (use-package which-key
   :ensure t
   :config (which-key-mode))
@@ -60,35 +64,7 @@
 
 (use-package dired-hide-dotfiles
   :ensure t
-  :config (dired-hide-dotfiles-mode))
-
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p))
-(setq neo-theme (if (display-graphic-p) 'icons 'icons))
-
-(use-package all-the-icons-dired
-  :ensure t
-  :hook (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
-
-(use-package nerd-icons
-  :ensure t)
-
-(use-package doom-themes
-  :ensure t
-  :config
-    (load-theme 'doom-one t))
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :config (progn
-	    (setq doom-modeline-env-version t)
-	    (setq auto-revert-check-vc-info t)
-	    (setq doom-modeline-total-line-number t)
-	    (if (facep 'mode-line-active)
-		(set-face-attribute 'mode-line-active nil :family "Segoe UI Black" :height 100)
-	      (set-face-attribute 'mode-line nil :family "Segoe UI Black" :height 100)n)))
+  :config (dired-hide-dotfiles-mode t))
 
 (use-package neotree
   :ensure t
@@ -113,13 +89,11 @@
 
 (use-package projectile
     :ensure t
-    :init (projectile-global-mode)
+    :init (projectile-mode t)
     :config (progn
 	      (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-	      (setq venv-location "C:/Users/lucas/AppData/Local/Programs/Python/")
-	      (setq venv-dirlookup-names '(".venv" "venv" ".env" "env" "pyenv" ".virtual"))
-	      (setq projectile-enable-caching t)))             
-    
+	      (setq projectile-enable-caching t)))
+
 (use-package ivy
   :ensure t
   :init (ivy-mode)
@@ -128,14 +102,67 @@
             (setq enable-recursive-minibuffers t)
             (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)))
 
+(use-package counsel-projectile
+  :ensure t
+  :config (counsel-projectile-mode t))
+
+;; Visual
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+(setq neo-theme (if (display-graphic-p) 'icons 'icons))
+
+(use-package all-the-icons-dired
+  :ensure t
+  :hook (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config (load-theme 'doom-one t))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :config (progn
+	    (setq doom-modeline-env-version t)
+	    (setq doom-modeline-total-line-number t)
+	    (if (facep 'mode-line-active)
+		(set-face-attribute 'mode-line-active nil :family "Segoe UI Black" :height 110)
+	      (set-face-attribute 'mode-line nil :family "Segoe UI Black" :height 100)))
+  :defines auto-revert-check-vc-info)
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :init (all-the-icons-ivy-rich-mode 1)
+  :config (setq all-the-icons-ivy-rich-icon t))
+
+(use-package ivy-rich
+  :ensure t
+  :init (ivy-rich-mode 1))
+
 
 ;; Python
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode t))
+
 (use-package elpy
   :ensure t
-  :init (elpy-enable))
+  :init (elpy-enable)
+(when (load "flycheck" t t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)))
+
 (add-hook 'elpy-mode-hook (lambda ()
                             (add-hook 'before-save-hook
                                       'elpy-black-fix-code nil t)))
+
+(use-package py-autopep8
+  :ensure t
+  :hook ((python-mode) . py-autopep8-mode))
 
 ;; inicializa o pacote
 (when (< emacs-major-version 27)
@@ -159,30 +186,39 @@
 (global-set-key (kbd "C-;") 'comment-line) ; comenta a linha
 
 ; Cria prefixo C-z
-(define-prefix-command 'C-z-map)
-(global-set-key (kbd "C-z") 'C-z-map)
+;; (define-prefix-command 'C-z-map)
+;; (global-set-key (kbd "C-z") 'C-z-map)
 
-; 
+; Undo all
+(defun undo-all ()
+  "Undo all edits."
+  (interactive)
+  (when (listp pending-undo-list)
+    (undo))
+  (while (listp pending-undo-list)
+    (undo-more 1))
+  (message "Buffer was completely undone"))
+
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-M-z") 'undo-all)
+
 
 ; Python
 (global-set-key (kbd "<f7>") (kbd "C-u C-c C-c")) ; elpy shortcut
 
-
-;;; Melpa stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-Iosvkem))
- '(custom-safe-themes
-   '("6f96a9ece5fdd0d3e04daea6aa63e13be26b48717820aa7b5889c602764cf23a" "8b148cf8154d34917dfc794b5d0fe65f21e9155977a36a5985f89c09a9669aa0" "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" "6f1f6a1a3cff62cc860ad6e787151b9b8599f4471d40ed746ea2819fcd184e1a" "d6b934330450d9de1112cbb7617eaf929244d192c4ffb1b9e6b63ad574784aad" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" default))
  '(package-selected-packages
-   '(doom-modeline all-the-icons-dired dired-hide-dotfiles dired doom-themes ivy projectile auto-virtualenv pytest blacken pyvenv pyenv magit elpy flycheck ace-window all-the-icons neotree which-key try))
- '(safe-local-variable-values '((eval venv-workon "elpy/rpc-venv/"))))
+   '(counsel-projectile all-the-icons-ivy-rich py-autopep8 which-key projectile neotree multiple-cursors magit ivy flycheck elpy doom-themes doom-modeline dired-hide-dotfiles auto-complete all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
